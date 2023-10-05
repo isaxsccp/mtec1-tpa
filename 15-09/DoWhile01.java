4b93d90e07e30c98418243ae1fc216bb3484e68a
al-mossar <143816039+isaxsccp@users.noreply.github.com>
2023-10-05 19:42:51 -0300

    DoWhile01

---
+++ 15-09/DoWhile01.java
@@ -0,0 +1,11 @@
+public class DoWhile01 {
+   public static void main(String[] args) {
+       int numero = 0;
+       do {
+           if (numero % 2 == 0) {
+               System.out.println(numero);
+           }
+           numero++;
+       } while (numero <= 500);
+   }
+}
