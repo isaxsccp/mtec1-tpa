34e19513af12a802c889a3843780ee1bafe7e12a
al-mossar <143816039+isaxsccp@users.noreply.github.com>
2023-10-05 19:57:26 -0300

    For03

---
+++ 15-09/For03.java
@@ -0,0 +1,11 @@
+public class For03 {
+   public static void main(String[] args) {
+       for (int i = 1; i <= 100; i++) {
+           if (i % 10 == 0) {
+               System.out.println("Múltiplo de 10");
+           } else {
+               System.out.println(i);
+           }
+       }
+   }
+}
