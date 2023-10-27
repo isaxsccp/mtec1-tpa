e4c3bc38ea41fa027f6946d72b229a16a607732d
isa <143816039+isaxsccp@users.noreply.github.com>
2023-10-27 08:35:44 -0300

    Ex03Vetores.java

---
+++ 20-10/Ex03Vetores.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ex03Vetores {
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	final int TAM=10;
+	int a[], b[], i, dim;
+	a = new int [TAM];
+	b = new int [TAM];
+	for (i=0; i<TAM; i++) {
+		System.out.println("Digite o "+(i+1)+"° valor de A");
+		a[i] = in.nextInt();
+	}
+	for (i=0; i<TAM; i++);
+
+	}
+ 
+}
