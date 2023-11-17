29c817885d4b148b519c59e596498446274e6ba5
isa <143816039+isaxsccp@users.noreply.github.com>
2023-11-17 00:51:01 -0300

    Ex2.java

---
+++ 20-10 (dezafios)/Ex2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Ex2 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int a[];
+        a = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Digite o numero " + (i + 1) + ": ");
+            a[i] = scanner.nextInt();
+            if (a[i] <= 1) {
+                System.out.println(a[i] + " não é primo");
+            } else {
+                int j;
+                for (j = 2; j < a[i]; j++) {
+                    if (a[i] % j == 0) {
+                        System.out.println(a[i] + " não é primo");
+                        break;
+                    }
+                }
+                if (j == a[i]) {
+                    System.out.println(a[i] + " é primo");
+                }
+            }
+        }
+    }
+}
