29db23e5687568ed7e40f75cfca9b865aa12f293
isa <143816039+isaxsccp@users.noreply.github.com>
2023-11-24 09:22:36 -0300

    Ex2.java
    Criar um vetor A com 5 elementos inteiros. Escreva um programa que imprima a tabuada de cada um dos elementos do vetor A.

--- 20-10 (dezafios)/Ex2.java
+++ 20-10 (dezafios)/Ex2.java
@@ -2,23 +2,23 @@ import java.util.Scanner;
 public class Ex2 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        int a[];
-        a = new int[10];
-        for (int i = 0; i < 10; i++) {
+        int a[]; //cria vetor a
+        a = new int[10]; //vetor a recebe valor de 10 posições
+        for (int i = 0; i < 10; i++) { //abre o laço q inicia o contador e enquanto o numero digitado for menor q 10 ele incrementa
             System.out.println("Digite o numero " + (i + 1) + ": ");
-            a[i] = scanner.nextInt();
-            if (a[i] <= 1) {
+            a[i] = scanner.nextInt(); //leu e incrementou tudo blz
+            if (a[i] <= 1) { //se os valores digitados forem menor ou igual a 1..
                 System.out.println(a[i] + " não é primo");
             } else {
-                int j;
-                for (j = 2; j < a[i]; j++) {
-                    if (a[i] % j == 0) {
+                int j; //senao (caso for maior)
+                for (j = 2; j < a[i]; j++) { //outo laço pra calcular se é primo
+                    if (a[i] % j == 0) { //se os valores digitados forem divisíveis por j..
                         System.out.println(a[i] + " não é primo");
                         break;
                     }
                 }
                 if (j == a[i]) {
-                    System.out.println(a[i] + " é primo");
+                    System.out.println(a[i] + " é primo"); //se j for igual ao numero digitado, tb é primo
                 }
             }
         }
